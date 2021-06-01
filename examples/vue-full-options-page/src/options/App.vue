5142b0d29eb257b4ab483e7cc9bc7fcfb0f24870
StarkShang <stark.shang@outlook.com>
2021-06-01 22:57:05 +0800

    feat: update examples

--- examples/vue-full-options-page/src/options/App.vue
+++ examples/vue-full-options-page/src/options/App.vue
@@ -1,5 +1,6 @@
 <template>
     <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>Hello, full options page!</h1>
 </template>
 
 <script lang="ts">
--- examples/vue-full-options-page/vite.config.ts
+++ examples/vue-full-options-page/vite.config.ts
@@ -4,13 +4,13 @@ import { chromeExtension } from "vite-plugin-chrome-extension";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: "src/manifest.json"
-        }
-    },
     plugins: [
-        vue(),
-        chromeExtension()
+        chromeExtension({
+            components: {
+                options: {
+                    plugins: [vue()]
+                }
+            }
+        })
     ]
 });
