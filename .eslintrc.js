11e23ddf224a1435a18cdba085435aad504c7ce2
Jason Pickens <jasonpickensnz@gmail.com>
2021-10-10 13:35:18 +1300

    Fix .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,7 @@ module.exports = {
             'warn',
             {
                 multiline: {
-                    delimiter: 'comma' | 'semi',
+                    delimiter: 'comma',
                     requireLast: true,
                 },
                 singleline: {
